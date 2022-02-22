505269a69a4474a33b2eb5e44bdf26a9762e65f5
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-02-22 23:37:35 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,27 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
+}
+
+TEST_CASE("gets breach values based on cooling type")
+{
+  REQUIRE(getTemperatureBreachValues(MED_ACTIVE_COOLING) == {0,40});
+}
+
+TEST_CASE("classifies temperature breach")
+{
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
+}
+            
+TEST_CASE("sending feed to controller based on breach value")
+{
+  REQUIRE(sendToController(TOO_HIGH) == "feed : 2");
+}
+
+TEST_CASE("Sending mail to recepient based on breach value")
+{
+  std::string recepient = "ab@c.com";
+  REQUIRE(sendToEmail(TOO_HIGH, recepient) == "To: ab@c.com .Hi, the temperature is too high");
 }
